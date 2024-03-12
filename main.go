552bec27ae0789ffd6dab796d1da429dbc55ebb8
Mike Gardner <mjgardner@abitofhelp.com>
2024-03-11 23:58:18 -0700

    before error group

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	mapset "github.com/deckarep/golang-set/v2"
 	"io/fs"
 	"os"
+	"runtime"
+	"sync"
 )
 
 // This application demonstrates the correct way to calculate an MD5 hash
@@ -14,7 +16,6 @@ import (
 	"github.com/abitofhelp/azmd5_hash_dir/hash/md5"
 	"github.com/abitofhelp/azmd5_hash_dir/hash/model"
 	"path/filepath"
-	"sort"
 )
 
 func WalkDirectory(
@@ -62,6 +63,42 @@ func WalkDirectory(
 	return localPaths, nil
 }
 
+func WalkDirectoryWithChannel(
+	root string,
+	excludeDirs mapset.Set[string],
+	excludeFiles mapset.Set[string],
+	c chan<- string) {
+
+	defer close(c)
+
+	fs.WalkDir(os.DirFS(root), ".", func(p string, d fs.DirEntry, err error) error {
+
+		filePath := root + "/" + p
+
+		if err != nil {
+			return fmt.Errorf("failed to walk the directory '%s': %w", filePath, err)
+		}
+
+		if excludeDirs.Contains(d.Name()) {
+			return filepath.SkipDir
+		}
+
+		// Scan any directories that are not in excludeDir.
+		if d.IsDir() {
+			return nil
+		}
+
+		// Skip any files that are in excludeFiles.
+		if !d.IsDir() && excludeFiles.Contains(d.Name()) {
+			return nil
+		}
+
+		// Calculate the base64 hash for a file of interest.
+		c <- filepath.Join(p)
+		return nil
+	})
+}
+
 func main() {
 	root := "/Users/mike/Downloads/clients/alm"
 	excludeDirs := mapset.NewSet[string]()
@@ -72,18 +109,54 @@ func main() {
 	excludeFiles := mapset.NewSet[string]()
 	excludeFiles.Add(".DS_Store")
 
-	if localPaths, err := WalkDirectory(root, excludeDirs, excludeFiles); err == nil {
-		paths := localPaths.ToSlice()
-		sort.Slice(paths, func(i, j int) bool {
-			return paths[i].PathInsideDirectory() < paths[j].PathInsideDirectory()
-		})
+	// find all files
+	c := make(chan string, runtime.NumCPU())
+	go WalkDirectoryWithChannel(root, excludeDirs, excludeFiles, c)
 
-		cnt := 0
-		for _, p := range paths {
-			cnt++
-			fmt.Printf("(%00d)%s\n%s\n\n", cnt, p.PathInsideDirectory(), p.AzureMd5())
-		}
-	} else {
-		panic(fmt.Errorf("failed to determine local paths in directory '%s': %w", root, err))
+	// hash all the files
+	wg := sync.WaitGroup{}
+	rc := make(chan model.LocalFile, runtime.NumCPU())
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for filePath := range c {
+
+				// Calculate the base64 hash for a file of interest.
+				if azureMd5, err := md5.GenMd5HashAsBase64(filepath.Join(root, filePath) { //; err == nil {
+					lf := model.NewLocalFile(filePath, azureMd5)
+					rc <- *lf
+				} //else {
+					//return fmt.Errorf("failed to generate a hash for '%s': %w", filePath, err)
+				//}
+			}
+		}()
 	}
+
+	// collect all the file hashes
+	m := make(map[string][]string)
+	go func() {
+		for r := range rc {
+			m[r.PathInsideDirectory()] = append(m[r.PathInsideDirectory()], r.AzureMd5())
+		}
+	}()
+	wg.Wait()
+	close(rc)
+
+	//paths := localPaths.ToSlice()
+	//sort.Slice(paths, func(i, j int) bool {
+	//	return paths[i].PathInsideDirectory() < paths[j].PathInsideDirectory()
+	//})
+
+	cnt := 0
+	for k, v := range m {
+		cnt++
+		fmt.Printf("(%00d)%s\n%s\n\n", cnt, k, v)
+	}
+
+	//
+	//for _, p := range paths {
+	//
+	//	fmt.Printf("(%00d)%s\n%s\n\n", cnt, p.PathInsideDirectory(), p.AzureMd5())
+	//}
 }
